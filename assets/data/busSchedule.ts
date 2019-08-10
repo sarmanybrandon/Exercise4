91454dba958c4732e086895a31ff7b0d9a3e7348
sarmanybrandon <52832686+sarmanybrandon@users.noreply.github.com>
2019-08-09 23:50:58 -0400

    exercise 4

---
+++ assets/data/busSchedule.ts
@@ -0,0 +1,14 @@
+import {BusSchedule} from '../../app/BusSchedule';
+
+export const BUSSCHEDULE: BusSchedule[] = [{
+ routes: 'Dundas',
+ timeArrive: '3:00pm',
+ timeLeave: '2:30pm'
+},
+{
+ routes: 'Trafalgar',
+ timeArrive: '3:00pm',
+ timeLeave: '2:30pm'
+}];
+
+
